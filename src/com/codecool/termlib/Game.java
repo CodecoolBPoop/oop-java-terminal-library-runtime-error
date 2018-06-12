d0d78df4b7a00d16f3300b76a9814da49edc18a3
Fábián János <fabian.janos93@gmail.com>
2018-06-12 11:03:09 +0000

    Skeleton of player

--- src/com/codecool/termlib/Game.java
+++ src/com/codecool/termlib/Game.java
@@ -1,4 +1,7 @@
 package com.codecool.termlib;
 
 public class Game {
+    public static void main(String[] args){
+        System.out.print("something");
+    }
 }
--- src/com/codecool/termlib/Player.java
+++ src/com/codecool/termlib/Player.java
@@ -1,4 +1,26 @@
 package com.codecool.termlib;
 
 public class Player {
+    private String name;
+    private int sumShipHealth;
+    private Board board;
+
+    public boolean checkWin(){
+       return False;
+    }
+    public boolean shoot(int x, int y){
+        return False;
+    }
+    public void placeShip(){
+
+    }
+    public void setName(String name){
+
+    }
+    public String getName(){
+
+    }
+
+
+
 }
