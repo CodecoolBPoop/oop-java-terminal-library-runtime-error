e7d190f76d5393eafa4f851e336f1511cf3285e2
Fábián János <fabian.janos93@gmail.com>
2018-06-12 14:01:55 +0000

    Game finished

--- src/com/codecool/termlib/Game.java
+++ src/com/codecool/termlib/Game.java
@@ -1,7 +1,35 @@
 package com.codecool.termlib;
 
 public class Game {
+    private void turn(Player current, Player opponent){
+        current.shoot();
+        if (current.checkWin()) {
+            System.out.print(player2.getName() + " Win");
+            break;
+        }
+    }
     public static void main(String[] args){
-        System.out.print("something");
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        int[] ships = new int[] {5,4,3,3,2};
+        for (int i=0; i< ships.length; i++){
+            player1.placeShip(i);
+        }
+        for (int i=0; i< ships.length; i++){
+            player2.placeShip(i);
+        }
+        while(true){
+            player1.shoot();
+            if (player1.checkWin()) {
+                System.out.print(player2.getName() + " Win");
+                break;
+            }
+            player2.shoot();
+            if (player2.checkWin()){
+                System.out.print(player1.getName() + " Win");
+                break;
+            }
+        }
     }
 }
