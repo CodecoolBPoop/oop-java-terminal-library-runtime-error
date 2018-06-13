56c1c07851305ece5467f20c05db3918418aa454
Fábián János <fabian.janos93@gmail.com>
2018-06-13 14:12:56 +0000

    Merge branch 'development' of https://github.com/CodecoolBPoop/oop-java-terminal-library-runtime-error into development
    # Conflicts:
    #	src/com/codecool/termlib/Board.java

--- src/com/codecool/termlib/Board.java
+++ src/com/codecool/termlib/Board.java
@@ -6,33 +6,38 @@ public class Board {
 
     int[][] board;
 
-    static private String abc = "  A B C D E F G H I J";
+    static private String abc = "   A B C D E F G H I J";
     static private String [] abcArray = abc.trim().split(" ");
 
     Board() {
-        board = new int[][] {   {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2},
-                                {2,2,2,2,2,2,2,2,2,2}};
+        board = createBoard(10, 10);
     }
 
     public Board(int width, int height) {
-        board = new int[height][width];
+        board = createBoard(width, height);
+    }
+
+    private static int[][] createBoard(int width, int height) {
+        int[][] tempBoard = new int[width][height];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                tempBoard[i][j] = 2;
+            }
+        }
+        return tempBoard;
     }
 
     void displayBoard(boolean ownBoard) {
         System.out.println(abc);
         for (int i = 0; i < board.length; i++) {
-            System.out.print(i + " ");
+            System.out.print(String.format("%1$2s", i + 1) + " ");
             for (int j = 0; j < board[i].length; j++) {
                 if (ownBoard) {
-                    System.out.print(board[i][j] + " ");
+                    if (board[i][j] == 2) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("#");
+                    }
                 } else {
                     switch (board[i][j]) {
                         case 1:
@@ -42,10 +47,10 @@ public class Board {
                             System.out.print("-");
                             break;
                         default:
-                            System.out.print(board[i][j]);
+                            System.out.print(" ");
                     }
-                    System.out.print(" ");
                 }
+                System.out.print(" ");
             }
             System.out.println();
         }
@@ -56,20 +61,16 @@ public class Board {
         while (true) {
             System.out.print("Please provide coordinates(e.g. A1, a1): ");
             String input = sc.nextLine();
-            if (input.length() == 2) {
-                try {
-                    char inputLetter = Character.toUpperCase(input.charAt(0));
-                    String inputStringLetter = String.valueOf(inputLetter);
-                    int col = java.util.Arrays.asList(abcArray).indexOf(inputStringLetter);
-                    int row = Character.getNumericValue(input.charAt(1));
-                    if (row <= 9 && col <= 9) {
-                        int[] returns = {row, col};
-                        return returns;
-                    }
-                } catch (Exception e) {
-                    continue;
+            try {
+                char inputLetter = Character.toUpperCase(input.charAt(0));
+                String inputStringLetter = String.valueOf(inputLetter);
+                int col = java.util.Arrays.asList(abcArray).indexOf(inputStringLetter);
+                int row = Integer.parseInt(input.substring(1)) - 1;
+                if (row <= 9 && row >= 0 && col <= 9 && col >= 0) {
+                    int[] returns = {row, col};
+                    return returns;
                 }
-            }
+            } catch (Exception e) {}
             System.out.println("Invalid coordinate");
         }
     }
