4dca7b49db14af199fdd89246d9765ba550d9698
Tibi <bukkfabiszti@gmail.com>
2018-06-14 13:28:27 +0200

    READY FOR DEPLOYMENT WOOHOO

--- src/com/codecool/termlib/Game.java
+++ src/com/codecool/termlib/Game.java
@@ -8,7 +8,7 @@ public class Game {
         Player player2 = new Player();
         player1.setName("Player 1");
         player2.setName("Player 2");
-        int[] ships = new int[] {5,4,3,3,2};
+        int[] ships = new int[] {5,4};
         for (int i=0; i< ships.length; i++){
             terminal.clearConsole();
             terminal.printLogo();
@@ -23,7 +23,6 @@ public class Game {
         while(true){
             terminal.clearConsole();
             terminal.printLogo();
-            player2.displayBoardToEnemy();
             System.out.print(player1.getName() + "'s turn\n");
             while (player2.shoot()){
                 if (player2.checkLose()) {
@@ -31,7 +30,8 @@ public class Game {
                     break game;
                 }
             }
-
+            terminal.clearConsole();
+            terminal.printLogo();
             System.out.print(player2.getName() + "'s turn\n");
             while (player1.shoot()){
                 if (player1.checkLose()){
